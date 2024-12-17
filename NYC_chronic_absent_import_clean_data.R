2a2d43e2134e76ef5554cc33f5b0ed4bacab3624
Alex J. Parton <parton.alexj@gmail.com>
2024-12-17 17:22:01 -0500

    attendance data clean and ready to export to csv.

--- NYC_chronic_absent_import_clean_data.R
+++ NYC_chronic_absent_import_clean_data.R
@@ -19,11 +19,6 @@ rm(list = ls())
 library(data.table)
 library(tidyverse)
 library(broom)
-# do I need these?
-library(pastecs)
-library(foreign)
-library(labelled)
-library(haven)
 
 ### Load Data ####
   # Load data in from .csv files
@@ -41,83 +36,90 @@ attendance_18_22_ELL <- read.csv("school_attendance_2018-2022_ELL.csv")
 attendance_18_22_STH <- read.csv("school_attendance_2018-2022_STH.csv")
 
   # list of data frames: add a column for demographic category and bind together
-data_frames <- list(attendance_18_22_all, attendance_18_22_SWD,attendance_18_22_ethnicity,
-                    attendance_18_22_gender, attendance_18_22_poverty, attendance_18_22_ELL, 
+data_frames <- list(attendance_18_22_all, attendance_18_22_ELL, attendance_18_22_ethnicity,
+                    attendance_18_22_gender, attendance_18_22_poverty, attendance_18_22_SWD,
                     attendance_18_22_STH)
   # names of demographic categories
-cat_names <- list("all_students", "SWD", "ethnicity", "gender", "poverty", "ELL", "STH")
+  # same order as data_frames listed above
+dem_cat_names <- list("All Students", "ELL Status", "Ethnicity", "Gender", "Poverty", "SWD Status",
+                  "STH Status")
   # function to add the demographic category
 add_category_column <- function(df, category) {
     df %>%
-      mutate(demographic_category = category)
+      mutate(Demographic.Category = category)
   }
   # apply the function to each data frame in the list
-modified_dfs <- modified_dfs <- map2(data_frames, cat_names, add_category_column)
+modified_dfs <- map2(data_frames, dem_cat_names, add_category_column)
   # bind everything together
 attendance_18_22 <- bind_rows(modified_dfs)
-
+  # change "Category" column to match older df: "Demographic.Variable"
+colnames(attendance_18_22)[4] <- "Demographic.Variable"
   # For merging these df's, we need to match up all the variables 
-table(attendance_18_22$Category) # this newer data includes a few more entries, e.g. "Neither male nor female"
-table(attendance_13_18$Demographic.Variable) # I will match to this so we have data for all years
+table(attendance_18_22$Demographic.Variable) # this newer data includes a few more entries, e.g. "Neither male nor female"
+table(attendance_13_18$Demographic.Variable) 
 
   # clean up (remove)
 rm(list=c("attendance_18_22_all", "attendance_18_22_ELL", "attendance_18_22_ethnicity", "attendance_18_22_ethnicity", 
           "attendance_18_22_gender", "attendance_18_22_gender", "attendance_18_22_poverty", "attendance_18_22_STH", 
-          "attendance_18_22_SWD", "add_category_column", "cat_names", "data_frames", 
+          "attendance_18_22_SWD", "add_category_column", "dem_cat_names", "data_frames", 
           "modified_dfs"))
-
   # remove extra categories from newer data
 attendance_18_22_trimmed <- attendance_18_22 %>% 
-  filter(!Category %in% c("Neither male nor female", "Not STH", "STH"))
+  filter(!Demographic.Variable %in% c("Neither male nor female")) # remove these because of low N's
   # check again
-table(attendance_18_22_trimmed$Category)
-table(attendance_13_18$Demographic.Variable) # the categories match up
-
+table(attendance_18_22_trimmed$Demographic.Variable) # newer data has STH and Not STH - OK. 
+table(attendance_13_18$Demographic.Variable) 
   # merge all years
-    # first, change name of "Demographic.Variable" to Category
-colnames(attendance_13_18)[6] <- "Category"  
     # check column names for both df's
 matrix(colnames(attendance_13_18))
 matrix(colnames(attendance_18_22_trimmed))
-    # remove "Demographic.Category" and "demographic category"
-    # also remove "X..Contributing.20..Total.Days", and "X..Contributing.10..Total.Days.and.1..Pres.Day"
-attendance_13_18 <- attendance_13_18[,c(-5,-11)]
-attendance_18_22_trimmed <- attendance_18_22_trimmed[,c(-10,-13)]
+    # remove "X..Contributing.20..Total.Days", and "X..Contributing.10..Total.Days.and.1..Pres.Day"
+attendance_13_18_finalvars <- attendance_13_18[,-11]
+attendance_18_22_finalvars <- attendance_18_22_trimmed[,-10]
     # check column names for both df's (again)
-matrix(colnames(attendance_13_18))
-matrix(colnames(attendance_18_22_trimmed))
+matrix(colnames(attendance_13_18_finalvars))
+matrix(colnames(attendance_18_22_finalvars))
     # rearrange to match columns
-attendance_18_22_trimmed <- attendance_18_22_trimmed[,c(1:3,5,4,6:11)] %>% 
-  filter(Year != "2018-19") # and remove overlapping year
+attendance_18_22_formerge <- attendance_18_22_finalvars[,c(1:3,5,12,4,6:11)] 
+    # remove 2018-19 from earlier data to retain STH variable for max years
+attendance_13_17_formerge <- attendance_13_18_finalvars %>% 
+  filter(Year != "2018-19") 
     # row bind together 
-attendance_13_22 <- rbind(attendance_13_18, attendance_18_22_trimmed)
-table(attendance_13_22$Year) # over 13,000 observations for each year
+attendance_13_22 <- rbind(attendance_13_17_formerge, attendance_18_22_formerge)
+table(attendance_13_22$Year) # over 13,000 observations for each year (more when STH is included in later years)
 table(attendance_13_22$Grade) # some schools have higher grades
-    # limit observations to grades K-5
+
+    #clean up (remove)
+rm(list=setdiff(ls(envir = .GlobalEnv), "attendance_13_22"), envir = .GlobalEnv)
+
+# SAVE this to a csv. for easy retrieval 
+write.csv(attendance_13_22, "clean_attendance_13_22_all_grades.csv")
+    
+  # limit observations to grades K-5
 attendance_13_22_k5 <- attendance_13_22 %>% 
   filter(Grade %in% c("0K", "1", "2", "3", "4","5")) %>% 
-  mutate(across(6:11, ~ gsub(",", "", .))) %>%  # Remove commas
-  mutate(across(6:11, as.numeric)) %>% # convert to numeric
-  arrange(DBN, Category, Year) # arrange data 
-    # clean up (remove)
-rm(list=c("attendance_13_18", "attendance_18_22", "attendance_18_22_trimmed"))
-    
-# test code
-test <-attendance_13_22_k5 %>% 
-  filter(Grade %in% c("0K", "1", "2", "3", "4","5")) %>% 
-  mutate(across(5:10, ~ gsub(",", "", .))) %>%  # Remove commas
-  mutate(across(5:10, as.numeric)) %>% 
-  arrange(DBN, Category, Year)
-test_pct <- test %>% 
-  group_by(DBN, Category, Year) %>% 
-  summarize(chronic_absent_pct = mean(`X..Chronically.Absent.1`, na.rm = T), 
+  mutate(across(7:12, ~ gsub(",", "", .))) %>%  # Remove commas
+  mutate(across(7:12, as.numeric)) %>% # convert to numeric
+  arrange(DBN,Year, Demographic.Category) # arrange data 
+
+# SAVE this to a csv. for easy retrieval
+write.csv(attendance_13_22_k5, "clean_attendance_13_22_k5.csv")
+  
+### Checkpoint ####
+
+# Calculate chronic absentee rates and put it in a dataframe
+  # test code
+
+test_pct <- attendance_13_22_k5 %>% 
+  group_by(DBN, School.Name, Year, Demographic.Category, Demographic.Variable) %>% 
+  # chronic absent percent and total per school, per year, per category and variable
+  summarize(chronic_absent_pct = round(mean(`X..Chronically.Absent.1`, na.rm = T),1), 
+            chronic_absent_n = sum(`X..Chronically.Absent`, na.rm=T), 
             .groups = 'drop') 
+  # need to add demographics so we can know total number of students
 
 
   
-rm(attendance_13_21)  
-
-
 ## DEMOGRAPHIC DATA 
 demographics_13_17 <- read.csv("demographic_snapshot_school_2013-2017.csv")
 # update this: new data that goes until year starting in  2023
